7f47a170bcf90cd8f509da9115c434fbcd03d2a7
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-03-09 21:18:13 -0300

    Styling

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
+import logo from '../../assets/logo.svg';
 
 export default function Signup() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
