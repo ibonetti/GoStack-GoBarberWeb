b8939b647ddff225d03ed1a04674d04a9fc22e94
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-04-13 20:17:44 -0300

    Notifications

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -37,6 +37,7 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 5px;
+  display: ${props => (props.visible ? 'block' : 'none')};
 
   &::before {
     content: '';
@@ -73,6 +74,8 @@ export const Notification = styled.div`
   time {
     font-size: 12px;
     opacity: 0.6;
+    display: block;
+    margin-bottom: 5px;
   }
 
   button {
@@ -80,10 +83,7 @@ export const Notification = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, '#7159c1')};
-    padding: 0 5px;
-    margin: 0 5px;
     cursor: pointer;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   ${props =>
@@ -96,6 +96,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
